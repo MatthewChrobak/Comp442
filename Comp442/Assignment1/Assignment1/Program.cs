d024fe6fa1944dc5f65aff7b0752158772784e27
Matthew Chrobak <matthewchrobak@hotmail.com>
2018-01-23 22:07:35 -0500

    Assignment 1

---
+++ Comp442/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using LexicalAnalyzer;
+
+namespace Assignment1
+{
+    public class Program
+    {
+        private const string _inputFile = "input.txt";
+
+        static Program()
+        {
+            if (!File.Exists(_inputFile)) {
+                File.Create(_inputFile).Close();
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            var tokenizer = new Tokenizer(File.ReadAllText(_inputFile));
+
+            Token token;
+            do {
+                token = tokenizer.NextToken();
+                Console.Write($"[{token.Type}:{token.TokenContent}:{token.SourceLocation}] ");
+            } while (token.Type != TokenType.EndOfStream);
+            Console.WriteLine();
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
